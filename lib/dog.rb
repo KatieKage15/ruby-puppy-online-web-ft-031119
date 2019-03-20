32f9aca6842927fcdf70b1c0d330667f281f2485
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-20 23:10:47 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  fido = Dog.new("Fido")
+  pluto = Dog.new("Pluto")
+  gonzo = Dog.new("Gonzo")
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    puts @@all.map { |dog| dog.name}
+  end
+
+  def self.clear_all
+  @@all.clear
+  end
+end
